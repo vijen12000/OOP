f20210c3448d0f24bc21321199b6bf5ddc936b64
vijen12000 <vijen12000@gmail.com>
2018-02-05 22:49:11 +0530

    FindCheapestPrinter iterator

---
+++ Painter/CheapestPainter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Painter
+{
+    class CheapestPainter
+    {
+        public static IPainter FindCheapestPrinter(double sqMeters,IEnumerable<IPainter> painters)
+        {
+            decimal bestPrice = 0;
+            IPainter cheapest = null;
+            //for each painter
+            foreach (var painter in painters)
+            {
+                if (painter.IsAvailable)
+                {
+                    var price = painter.EstimateCost(sqMeters);
+                    if (cheapest ==null || bestPrice < price)
+                    {
+                        cheapest = painter;
+                    }
+                }
+            }
+            return cheapest;
+        }
+    }
+}
---
+++ Painter/IPainter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Painter
+{
+    interface IPainter
+    {
+       bool IsAvailable { get; }
+        TimeSpan EstimateTimeToPaint(double sqMeters);
+        decimal EstimateCost(double sqMeters);
+    }    
+}
