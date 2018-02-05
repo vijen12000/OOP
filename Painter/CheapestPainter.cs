40ff1df77b6619b28af7749d6879de86cabaf277
vijen12000 <vijen12000@gmail.com>
2018-02-05 23:37:27 +0530

    /// Use of extension Method

--- Painter/CheapestPainter.cs
+++ Painter/CheapestPainter.cs
@@ -6,8 +6,60 @@ using System.Threading.Tasks;
 
 namespace Painter
 {
+    public static class EnumerableExtensions
+    {
+        public static T WithMinimum<T, TKey>(this IEnumerable<T> sequence, Func<T, TKey> predicate)
+                where T : class
+                where TKey : IComparable<TKey>
+                => sequence.Aggregate((T)null, (best, cur) => best == null || predicate(cur).CompareTo(predicate(best))<0 ?
+                    best : cur);
+    }
     class CheapestPainter
     {
+        /// <summary>
+        /// Use of extension Method
+        /// </summary>
+        /// <param name="sqMeters"></param>
+        /// <param name="painters"></param>
+        /// <returns></returns>
+        public static IPainter FindCheapestPrinter_v4(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return
+                painters
+                .Where(w => w.IsAvailable)
+                .WithMinimum(p=>p.EstimateCost(sqMeters));
+        }
+
+        /// <summary>
+        /// With Aggrigate but estimate function is called with each iteration twice, slightly better then v3
+        /// </summary>
+        /// <param name="sqMeters"></param>
+        /// <param name="painters"></param>
+        /// <returns></returns>
+        public static IPainter FindCheapestPrinter_v3(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return
+                painters
+                .Where(w => w.IsAvailable)
+                .Aggregate((IPainter)null,(best, cur) => best==null || best.EstimateCost(sqMeters) < cur.EstimateCost(sqMeters) ?
+                  best : cur);
+        }
+
+        /// <summary>
+        /// With Aggrigate but estimate function is called with each iteration twice, slightly better then v2, but can be improved
+        /// and throws exception if sequence is empty
+        /// </summary>
+        /// <param name="sqMeters"></param>
+        /// <param name="painters"></param>
+        /// <returns></returns>
+        public static IPainter FindCheapestPrinter_v3(double sqMeters, IEnumerable<IPainter> painters)
+        {            
+            return
+                painters
+                .Where(w => w.IsAvailable)
+                .Aggregate((best,cur)=>best.EstimateCost(sqMeters) < cur.EstimateCost(sqMeters) ?
+                  best:cur);
+        }
 
         public static IPainter FindCheapestPrinter_v2(double sqMeters, IEnumerable<IPainter> painters)
         {
